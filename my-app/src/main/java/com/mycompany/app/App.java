73bddfe66e673817b1cb0fa22c9ef06ea2dc2092
Gledson Luiz Oliveira <gledson.oliveira@verity.com.br>
2018-10-02 13:27:43 -0300

    alteracao para nexus

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,29 +1,21 @@
 package com.mycompany.app;
 
+import java.util.*;
+
 /**
- * Hello world!
- *
+ * Projeto: POC Pipeline portal
+ * ANO: 2018
+ * Empresa: Verity Group
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Calendar c = Calendar.getInstance();
+    	System.out.println("Hello World! " + c.get(Calendar.DATE));
 
-        System.out.println("A NullPointerException is a drag...");
-        Object myObject = null;
-        /**
-         * Checker prevents this from compiling...
-         */
-//        System.out.println("myObject: " + myObject.toString());
-        /**
-         * ... which is simply fantastic. It shows:
-         *
-         * error: [dereference.of.nullable] dereference of possibly-null
-         * reference myObject
-         *
-         * http://checkerframework.org
-         */
-        System.out.println("... but thankfully, Checker has our back: http://checkerframework.org");
+    	System.out.println("Controle de qualidade e Pipeline " + c.get(Calendar.DAY_OF_WEEK));
+    	
+        System.out.println("Verity Group " + c.get(Calendar.YEAR));
     }
 }
